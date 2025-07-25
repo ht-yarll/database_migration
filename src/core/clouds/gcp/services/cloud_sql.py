8f1ec7f7e62a0c081b8db6b0f84bc2af9cf30083
ht-yarll <htorres5@outlook.com>
2025-07-25 19:19:59 -0300

    :construction:

--- src/core/clouds/gcp/services/cloud_sql.py
+++ src/core/clouds/gcp/services/cloud_sql.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 
@@ -7,7 +8,9 @@ class CloudSQLService:
 
     def export_mysql_gcp(self):
         """Exporta o banco para um file chamado 'backup.sql' usando mysqldump"""
-        dump_file = "backup.sql"
+        tmp_dir = self.config["migrations"]["tmp_dir"]
+        os.makedirs(tmp_dir, exist_ok=True)  # Garante que o diretório existe
+        dump_file = tmp_dir + "/backup.sql"
         try:
             subprocess.run(
                 f"mysqldump -h {self.config['cloud_sql']['instance_name']} -u {self.config['cloud_sql']['user']} -p{self.config['cloud_sql']['password']} "
--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from src.config.load_config import load_config
-from src.core.clouds.aws.aws import AWSHelper
-from src.core.clouds.gcp.gcp import GCPHelper
 
 
 def main():
-    config = load_config()
     ...
+
+        
