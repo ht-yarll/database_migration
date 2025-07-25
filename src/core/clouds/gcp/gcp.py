0700327e54b50e103743a6bbb549186f3aefb0af
ht-yarll <htorres5@outlook.com>
2025-07-25 16:09:31 -0300

    :heavy_plus_sign:feat; gcp and cloud_sql modules

---
+++ src/core/clouds/gcp/gcp.py
@@ -0,0 +1,14 @@
+from services.cloud_sql import CloudSQLService
+
+class GCP:
+    def __init__(self, config: dict):
+        self.config = config
+    
+    def fetch_backup_on_gcp_db(self):
+        try:
+            cloud_sql= CloudSQLService(self.config)
+            cloud_sql.export_mysql_gcp()
+
+        except Exception as e:
+            print(f"An error occurred while initializing GCP services: {e}")
+            raise e
